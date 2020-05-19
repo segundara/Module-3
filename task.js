079360ba3d831627971d04743c16dda19fee62ef
segundara <segundara@gmail.com>
2020-05-19 18:07:32 +0300

    music from api

--- task.js
+++ task.js
@@ -205,4 +205,32 @@ function anagram2(word, list){
 
 let word = ('listen')
 let list = ('enlists, google, inlets, banana')
-console.log(anagram2(word,list))
+//console.log(anagram2(word,list))
+
+// 4) PALINDROME
+
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+
+// --- Examples:
+
+//     palindrome("abba") === true
+//     palindrome("abcdefg") === false
+
+function palindrome(str) {
+
+    var len = str.length;
+    var mid = Math.floor(len/2);
+
+    for ( var i = 0; i < mid; i++ ) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(palindrome('abba'))
